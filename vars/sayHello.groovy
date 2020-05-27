4cd673b19dcc78009e7588fc604811172d625d93
pmnathan <prakash@hashicorp.com>
2020-05-27 13:23:16 -0400

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,15 @@
 #!/usr/bin/env groovy
 
 
-def call(String name = 'human') {
+def call(String name = 'human', String TFC_TOKEN) {
   echo "Hello, ${name}."
   echo "Hello, test123."
-  def get = new URL("https://httpbin.org/get").openConnection();
+  echo "TFC_TOKEN, ${TFC_TOKEN}"
+
+  def get = new URL("https://app.terraform.io/api/v2/organizations/fanniemae_pov/workspaces/workspace-1").openConnection();
+  post.setRequestProperty("Content-Type", "application/vnd.api+json")
+  post.setRequestProperty("Authorization", "Bearer ${TFC_TOKEN}")
+  
   def getRC = get.getResponseCode();
   println(getRC);
   if(getRC.equals(200)) {
