08caad1d30ac2a80c50fff1c7e7714561fa137ba
pmnathan <prakash@hashicorp.com>
2020-05-27 12:36:32 -0400

    test

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,5 +4,12 @@
 def call(String name = 'human') {
   echo "Hello, ${name}."
   echo "Hello, test123."
+  def get = new URL("https://httpbin.org/get").openConnection();
+  def getRC = get.getResponseCode();
+  println(getRC);
+  if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+  }
+
 
 }
